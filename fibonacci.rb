6ef32bd4d9ec82d2b8c2895d80d36ef9d4f91b7e
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-05-02 23:35:41 -0400

    Added fibonnaci recursive

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,15 +1,24 @@
 def fibs(number)
-    array = []
-  
-    for i in 0...number
-      if i == 0
-        array << 0
-      elsif i == 1
-        array << 1
-      else
-        array << array[i-1] + array[i-2]
-      end
+  array = []
+
+  for i in 0...number
+    if i == 0
+      array << 0
+    elsif i == 1
+      array << 1
+    else
+      array << array[i-1] + array[i-2]
     end
-  
-    array
+  end
+
+  array
+end
+
+def fibs_rec(number)
+  return [] if number == 0
+  return [0] if number == 1
+  return [0, 1] if number == 2
+
+  sequence = fibs_rec(number - 1)
+  sequence << sequence[-1] + sequence[-2]
 end
